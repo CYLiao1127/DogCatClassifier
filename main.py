1fcf0d39719f49f927133f88044f11187246fe12
liao <liao881127@gmail.com>
2025-07-23 15:47:38 +0800

    add argparse

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import os
+import argparse
 from config import Config
 from data.dataloader import get_dataloaders
 from model.model import DogCatModel
 from train import train
-import os
 
 if __name__ == '__main__':
+    # ---- Parse command-line arguments ----
+    parser = argparse.ArgumentParser(description='Dog vs Cat Classification')
+    # parser.add_argument('--mode', type=str, default='train', help='Mode: train / eval / predict')
+    parser.add_argument('--model_name', type=str, help='Model architecture (e.g. resnet18, resnet50)')
+    parser.add_argument('--batch_size', type=int, help='Batch size for training')
+    parser.add_argument('--img_size', type=int, help='Input image size (e.g. 224)')
+    parser.add_argument('--epochs', type=int, help='Number of training epochs')
+    args = parser.parse_args()
+
+    # ---- Load config and override ----
     config = Config()
+    if args.model_name:
+        config.model_name = args.model_name
+    if args.batch_size:
+        config.batch_size = args.batch_size
+    if args.img_size:
+        config.img_size = args.img_size
+    if args.epochs:
+        config.epochs = args.epochs
+
+    # ----- prepare data and model ----
     train_loader, val_loader = get_dataloaders(
         data_dir=config.data_dir,
         img_size=config.img_size,
